7ae2733b9bcc2a4a4b659cc20e33636f2e8462d4
Natalia Tejedor <natalia.tejedor@krc.es>
2017-09-22 12:30:17 +0200

    First commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='iXsystems Installer',
+      version='0.6',
+      description='iXSystems OS Installation',
+      author='Sean Eric Fagan',
+      author_email='sef@ixsystems.com',
+      packages=['ixsystems', 'ixsystems.installer'],
+)
